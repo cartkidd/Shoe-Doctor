cddb0e2984d8addcdeeb148038c570fa3a26f73c
Cartrell KIDD <cartrellk56@gmail.com>
2022-02-03 15:29:08 -0500

    updated users

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,13 +1,107 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { User, Shoe } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('shoes')
+          .populate('friends');
+
+        return userData;
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('shoes')
+        .populate('friends');
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('friends')
+        .populate('shoes');
+    },
     shoes: async (parent, { username }) => {
       const params = username ? { username } : {};
       return Shoe.find(params).sort({ createdAt: -1 });
+    },
+    shoe: async (parent, { _id }) => {
+      return Shoe.findOne({ _id });
+    }
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addShoe: async (parent, args, context) => {
+      if (context.user) {
+        const shoe = await Shoe.create({ ...args, username: context.user.username });
+
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { thoughts: shoe._id } },
+          { new: true }
+        );
+
+        return shoe;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    addReaction: async (parent, { shoeId, reactionBody }, context) => {
+      if (context.user) {
+        const updatedShoe = await Shoe.findOneAndUpdate(
+          { _id: shoeId },
+          { $push: { reactions: { reactionBody, username: context.user.username } } },
+          { new: true, runValidators: true }
+        );
+
+        return updatedShoe;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    addFriend: async (parent, { friendId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { friends: friendId } },
+          { new: true }
+        ).populate('friends');
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
     }
   }
 };
-
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,30 +1,52 @@
-// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
 const typeDefs = gql`
-type Shoe {
-  _id: ID
-  shoeText: String
-  createdAt: String
-  username: String
-  reactionCount: Int
-},
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    shoes: [Shoe]
+    friends: [User]
+  }
 
-type Reaction {
-  _id: ID
-  reactionBody: String
-  createdAt: String
-  username: String
+  type Shoe {
+    _id: ID
+    shoeText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  type Auth {
+     token: ID!
+     user: User
+   
 }
 
-
-
   type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
     shoes(username: String): [Shoe]
+    shoe(_id: ID!): Shoe
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addShoe(shoeText: String!): Shoe
+    addReaction(shoeId: ID!, reactionBody: String!): Shoe
+    addFriend(friendId: ID!): User
   }
 `;
 
-
-// export the typeDefs
 module.exports = typeDefs;
